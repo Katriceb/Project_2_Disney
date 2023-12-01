48c57e54a91be74d901302d4e74e07d5d97280d1
Katriceb <birch5trice@gmail.com>
2023-12-01 10:50:33 -0500

    comment

--- README.md
+++ README.md
@@ -15,6 +15,8 @@
 5.   Initial display of data on browser after fetch
 6.   Styling
 
+deployed link
+https://6569dd6223ead5238489ed9f--curious-gumdrop-22fdcc.netlify.app/about
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/Page/About.jsx
+++ src/Page/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div><p>This page aims to celebrate the beloved characters from</p> <p>various Disney movies, shows, and franchises,</p> <p>offering a centralized hub for fans to reminisce,</p> <p>discover and engage with their favorite characters</p></div>
+    <div><p>This page aims to celebrate the beloved characters from</p>
+     <p>various Disney movies, shows, and franchises,</p> <p>offering a centralized hub for fans to reminisce,</p> 
+     <p>discover and engage with their favorite characters</p></div>
   )
 }
 
